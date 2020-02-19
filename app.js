c3fb65dc4dea6b8c63f0d250be3b6abdbfcae04d
harika <harikapuppala19@gmail.com>
2020-02-19 21:18:02 +0530

    changes committed

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-var express = require("express")
-var app = express()
+var express = require("express");
+var app = express();
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); 
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res){
-   res.render("home");
+app.get("/", function(req, res) {
+    res.render("home");
 });
 
 app.get("/register", (req, res) => {
     res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    let details = req.body;
+    res.render("show", { aa: details });
 });
 
 app.get("/login", (req, res) => {
@@ -25,16 +30,29 @@ app.get("/booksearch", (req, res) => {
     res.render("booksearch");
 });
 
+app.post("/booksearch", function(req, res) {
+    var q = [
+        {
+            name: "Rahul",
+            id: 1
+        },
+        {
+            name: "Ritu",
+            id: 2
+        },
+        {
+            name: "Harika",
+            id: 3
+        }
+    ];
+    let booksearch = req.body;
+    res.render("booksearchshow", { bb: booksearch });
+});
+
 app.get("/bookissue", (req, res) => {
     res.render("bookissue");
 });
 
-app.get("/login", )
-
-app.post("/register", function(req, res){
-     console.log(req.body);
-});
-
-app.listen(3000, function(req, res){
+app.listen(3000, function(req, res) {
     console.log("Server started");
 });
