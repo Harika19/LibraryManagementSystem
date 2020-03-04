aafcb551f440010c8159f2a74612d7d88001cf4c
harika <harikapuppala19@gmail.com>
2020-03-04 23:49:26 +0530

    added mysql

--- app.js
+++ app.js
@@ -1,5 +1,85 @@
 var express = require("express");
+
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "harika19",
+    database: "libdb"
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('mysql connected');
+});
+
 var app = express();
+//create db
+app.get("/createdb", (req, res) => {
+    let sql = 'CREATE DATABASE libdb';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('database created...');
+    });
+});
+
+//create table
+app.get("/createtable", (req, res) => {
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY (id))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('table created...');
+
+    });
+});
+
+//insert data
+app.get("/insert1", (req, res) => {
+    let post = { name: 'DENNIS RICHIE' };
+    let sql = 'INSERT INTO posts SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('data 1 inserted in table....');
+
+    });
+});
+
+app.get("/insert2", (req, res) => {
+    let post = { name: 'BALAGURUSAMY' };
+    let sql = 'INSERT INTO posts SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('data 2 inserted in table....');
+
+    });
+});
+
+app.get("/insert3", (req, res) => {
+    let post = { name: 'ANDREW NG' };
+    let sql = 'INSERT INTO posts SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('data 3 inserted in table....');
+
+    });
+});
+
+//select single post
+app.get("/single/:id", (req, res) => {
+    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, results) => {
+        if (err) throw err;
+        console.log(results);
+    });
+});
 
 
 
@@ -86,38 +166,6 @@ app.post("/bookissue", function (req, res) {
 });
 
 
-app.listen(3000, function (req, res) {
+app.listen(5000, function (req, res) {
     console.log("Server started");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
