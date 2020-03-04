0b0089bfa5c66dec226bb429ccbf94d36ec2ef08
harika <harikapuppala19@gmail.com>
2020-03-04 23:44:06 +0530

    commited successfully

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 
+
+
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,7 +11,7 @@ app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.render("home");
 });
 
@@ -17,7 +19,7 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
-app.post("/register", function(req, res) {
+app.post("/register", function (req, res) {
     let details = req.body;
     res.render("show", { aa: details });
 });
@@ -26,22 +28,28 @@ app.get("/login", (req, res) => {
     res.render("login");
 });
 
+
+app.post("/login", function (req, res) {
+    let details = req.body;
+    res.render("showlog");
+});
+
 app.get("/booksearch", (req, res) => {
     res.render("booksearch");
 });
 
-app.post("/booksearch", function(req, res) {
+app.post("/booksearch", function (req, res) {
     var q = [
         {
-            name: "Rahul",
+            name: "DENNIS RICHIE",
             id: 1
         },
         {
-            name: "Ritu",
+            name: "BALAGURUSAMY",
             id: 2
         },
         {
-            name: "Harika",
+            name: "ANDREW NG",
             id: 3
         }
     ];
@@ -54,6 +62,62 @@ app.get("/bookissue", (req, res) => {
     res.render("bookissue");
 });
 
-app.listen(3000, function(req, res) {
+app.post("/bookissue", function (req, res) {
+    var w = [
+        {
+            name: "DENNIS RICHIE",
+            id: 1,
+            count: 5
+        },
+        {
+            name: "BALAGURUSAMY",
+            id: 2,
+            count: 5
+        },
+        {
+            name: "ANDREW NG",
+            id: 3,
+            count: 5
+        }
+    ];
+    let bookissue = req.body;
+    console.log(bookissue);
+    res.render("bookissueshow", { bb: bookissue, dataw: w });
+});
+
+
+app.listen(3000, function (req, res) {
     console.log("Server started");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
