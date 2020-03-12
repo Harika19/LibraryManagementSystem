fec2fbd57f781167d584ab72f8aeadd99228b2c3
Harika19 <36102187+Harika19@users.noreply.github.com>
2020-03-12 17:05:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
 var express = require("express");
 
-var mysql = require('mysql');
-
-var db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "harika19",
-    database: "libdb"
-});
-
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('mysql connected');
-});
-
+var bookRoute = require("./routes/books");
 var app = express();
+var db = require("./models/index");
+
 //create db
 app.get("/createdb", (req, res) => {
-    let sql = 'CREATE DATABASE libdb';
+    let sql = 'CREATE DATABASE libdb' ;
     db.query(sql, (err, result) => {
-        if (err) throw err;
+        if(err) throw err;
         console.log(result);
         res.send('database created...');
     });
@@ -29,9 +16,9 @@ app.get("/createdb", (req, res) => {
 
 //create table
 app.get("/createtable", (req, res) => {
-    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY (id))';
-    db.query(sql, (err, result) => {
-        if (err) throw err;
+    let sql = 'CREATE TABLE posting(id int AUTO_INCREMENT, name VARCHAR(255),count int, PRIMARY KEY (id))';
+    db.query(sql, (err,result) =>{
+        if(err) throw err;
         console.log(result);
         res.send('table created...');
 
@@ -40,10 +27,10 @@ app.get("/createtable", (req, res) => {
 
 //insert data
 app.get("/insert1", (req, res) => {
-    let post = { name: 'DENNIS RICHIE' };
-    let sql = 'INSERT INTO posts SET ?';
+    let post = {name: 'DENNIS RICHIE', count: '5'};
+    let sql = 'INSERT INTO posting SET ?';
     let query = db.query(sql, post, (err, result) => {
-        if (err) throw err;
+        if(err) throw err;
         console.log(result);
         res.send('data 1 inserted in table....');
 
@@ -51,10 +38,10 @@ app.get("/insert1", (req, res) => {
 });
 
 app.get("/insert2", (req, res) => {
-    let post = { name: 'BALAGURUSAMY' };
-    let sql = 'INSERT INTO posts SET ?';
+    let post = {name: 'BALAGURUSAMY', count: '5'};
+    let sql = 'INSERT INTO posting SET ?';
     let query = db.query(sql, post, (err, result) => {
-        if (err) throw err;
+        if(err) throw err;
         console.log(result);
         res.send('data 2 inserted in table....');
 
@@ -62,26 +49,26 @@ app.get("/insert2", (req, res) => {
 });
 
 app.get("/insert3", (req, res) => {
-    let post = { name: 'ANDREW NG' };
-    let sql = 'INSERT INTO posts SET ?';
+    let post = {name: 'ANDREW NG', count: '5'};
+    let sql = 'INSERT INTO posting SET ?';
     let query = db.query(sql, post, (err, result) => {
-        if (err) throw err;
+        if(err) throw err;
         console.log(result);
         res.send('data 3 inserted in table....');
 
     });
 });
 
-//select single post
-app.get("/single/:id", (req, res) => {
-    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err, results) => {
-        if (err) throw err;
+//select all  post
+ app.get("/bookapi", (req, res) => {
+    let sql = `SELECT * FROM posting`;
+    let query = db.query(sql, (err, results) =>{
+        if(err) throw err;
         console.log(results);
-    });
+        res.json(results);
+        
 });
-
-
+        });
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
@@ -91,81 +78,9 @@ app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
-    res.render("home");
-});
+app.use("/", bookRoute);
 
-app.get("/register", (req, res) => {
-    res.render("register");
-});
-
-app.post("/register", function (req, res) {
-    let details = req.body;
-    res.render("show", { aa: details });
-});
-
-app.get("/login", (req, res) => {
-    res.render("login");
-});
-
-
-app.post("/login", function (req, res) {
-    let details = req.body;
-    res.render("showlog");
-});
-
-app.get("/booksearch", (req, res) => {
-    res.render("booksearch");
-});
-
-app.post("/booksearch", function (req, res) {
-    var q = [
-        {
-            name: "DENNIS RICHIE",
-            id: 1
-        },
-        {
-            name: "BALAGURUSAMY",
-            id: 2
-        },
-        {
-            name: "ANDREW NG",
-            id: 3
-        }
-    ];
-    let booksearch = req.body;
-    console.log(booksearch);
-    res.render("booksearchshow", { bb: booksearch, datas: q });
-});
-
-app.get("/bookissue", (req, res) => {
-    res.render("bookissue");
-});
-
-app.post("/bookissue", function (req, res) {
-    var w = [
-        {
-            name: "DENNIS RICHIE",
-            id: 1,
-            count: 5
-        },
-        {
-            name: "BALAGURUSAMY",
-            id: 2,
-            count: 5
-        },
-        {
-            name: "ANDREW NG",
-            id: 3,
-            count: 5
-        }
-    ];
-    let bookissue = req.body;
-    console.log(bookissue);
-    res.render("bookissueshow", { bb: bookissue, dataw: w });
-});
-
-
-app.listen(5000, function (req, res) {
+app.listen(5000, function(req, res) {
     console.log("Server started");
 });
+ 
